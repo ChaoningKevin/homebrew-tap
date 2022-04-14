37d456e7e5b8b2ef0095e6f898d98654387511ab
Li <cnkevin@amazon.com>
2022-04-14 15:00:02 -0700

    Change reference

--- Formula/shinysylveon.rb
+++ Formula/shinysylveon.rb
@@ -5,7 +5,7 @@ class Shinysylveon < Formula
   sha256 "e18fbef2ab6e710fa3edb8ad8cbbb128bbe9a9697f220c4f37b4fb3de43a45cc"
   license "Apache-2.0"
 
-  depends_on "chaoningkevin/homebrew-tap/qemu@5.1.0"
+  depends_on "chaoningkevin/homebrew-tap/qemu@1.0"
 
   def install
     puts "Hello Kevin!"
