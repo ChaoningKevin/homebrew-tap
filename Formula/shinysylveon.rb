4d5986cbfa3b858116cd1aa8056c2bab50c46744
Li <cnkevin@amazon.com>
2022-04-14 14:39:43 -0700

    Change reference

--- Formula/shinysylveon.rb
+++ Formula/shinysylveon.rb
@@ -5,7 +5,7 @@ class Shinysylveon < Formula
   sha256 "e18fbef2ab6e710fa3edb8ad8cbbb128bbe9a9697f220c4f37b4fb3de43a45cc"
   license "Apache-2.0"
 
-  depends_on "anqipang/homebrew-podman/podman@3.4.4"
+  depends_on "chaoningkevin/homebrew-tap/qemu_1.0"
 
   def install
     puts "Hello Kevin!"
