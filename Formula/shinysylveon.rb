55cc07d193d164549bdfedc14583573ad00706e1
Li <cnkevin@amazon.com>
2022-04-14 12:18:31 -0700

    Add formula

---
+++ Formula/shinysylveon.rb
@@ -0,0 +1,13 @@
+class Shinysylveon < Formula
+  desc "Kevin's homebrew tap to test ruby dependency and version management"
+  homepage ""
+  url "https://github.com/ChaoningKevin/ShinySylveon/archive/refs/tags/v1.0.tar.gz"
+  sha256 "e18fbef2ab6e710fa3edb8ad8cbbb128bbe9a9697f220c4f37b4fb3de43a45cc"
+  license "Apache-2.0"
+
+  depends_on "qemu"
+
+  def install
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+  end
+end
