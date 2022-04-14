ecd1286ab38104090c3e847debf82b90ee4c05f8
Li <cnkevin@amazon.com>
2022-04-14 14:05:23 -0700

    Change reference

--- Formula/shinysylveon.rb
+++ Formula/shinysylveon.rb
@@ -5,7 +5,7 @@ class Shinysylveon < Formula
   sha256 "e18fbef2ab6e710fa3edb8ad8cbbb128bbe9a9697f220c4f37b4fb3de43a45cc"
   license "Apache-2.0"
 
-  depends_on "https://github.com/ChaoningKevin/ShinySylveonDependencies/blob/main/v1.0/qemu.rb"
+  depends_on "qemu_1.0"
 
   def install
     puts "Hello Kevin!"
