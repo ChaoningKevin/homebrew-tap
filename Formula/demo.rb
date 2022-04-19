435998d0f3143c3d3c5a16e5fd70cec608d153da
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-04-19 13:33:18 -0700

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -71,7 +71,7 @@ class Demo < Formula
     
     resource("qemu").stage{
       system "./configure", *args}
-    resource("qemu").stage(echo "Installing")
+    resource("qemu").stage(system "echo", "Installing")
   
     puts "Hello Kevin!"
     puts "ShinySylveon demo installed"
