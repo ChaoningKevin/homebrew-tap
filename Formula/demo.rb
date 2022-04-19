d2dac9de3ee85179bd53cfb748629be9065341a2
Anqi Pang <55934700+AnqiPang@users.noreply.github.com>
2022-04-19 13:28:59 -0700

    Update demo.rb

--- Formula/demo.rb
+++ Formula/demo.rb
@@ -9,6 +9,35 @@ class Demo < Formula
     url "https://download.qemu.org/qemu-7.0.0-rc4.tar.xz"
     sha256 "be0cfbb301a969a6242826a069f9c441ddf3620fa28861214a647d581d599aaa" 
   end 
+  
+  depends_on "libtool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libslirp"
+  depends_on "libssh"
+  depends_on "libusb"
+  depends_on "lzo"
+  depends_on "ncurses"
+  depends_on "nettle"
+  depends_on "pixman"
+  depends_on "snappy"
+  depends_on "vde"
+
+  on_linux do
+    depends_on "attr"
+    depends_on "gcc"
+    depends_on "gtk+3"
+    depends_on "libcap-ng"
+  end
+
+  fails_with gcc: "5"
+
 
   def install
     bin.install "src/HelloWorld.go" 
