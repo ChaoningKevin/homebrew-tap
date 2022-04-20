0edaf881d4964b366450dedc3f59d674033071ff
Anqi Pang <anqipans@amazon.com>
2022-04-20 16:29:23 -0700

    url

--- Formula/demo-private.rb
+++ Formula/demo-private.rb
@@ -1,7 +1,7 @@
 require_relative "lib/private"
 class DemoPrivate < Formula
   desc "Kevin's homebrew tap to test ruby dependency and version management"
-  homepage ""
+  homepage "https://github.com/AnqiPang/demo-private/releases/download/v1.0.1/demo-private-1.0.1.tar.gz"
   url "https://github.com/AnqiPang/demo-private/archive/refs/tags/v1.0.1.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
   sha256 "15d9fc76c316e6b4149a3cb7c3863735af1421603c719955ca5af8c84f5707ec"
   license "Apache-2.0"
