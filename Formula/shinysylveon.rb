f142d760a883ea6d7edc7a2834aec77e783fdb43
Anqi Pang <anqipans@amazon.com>
2022-04-21 09:29:39 -0700

    update

--- Formula/shinysylveon.rb
+++ Formula/shinysylveon.rb
@@ -1,13 +1,14 @@
 class Shinysylveon < Formula
   desc "Kevin's homebrew tap to test ruby dependency and version management"
   homepage ""
-  url "https://github.com/ChaoningKevin/ShinySylveon/archive/refs/tags/v1.0.tar.gz"
-  sha256 "e18fbef2ab6e710fa3edb8ad8cbbb128bbe9a9697f220c4f37b4fb3de43a45cc"
+  url "https://github.com/ChaoningKevin/ShinySylveon/archive/refs/tags/v3.1.tar.gz"
+  sha256 "433ec2326f08aaf72f3fb4a6faa064b0b4d691a1b4e4aa05a09a20b513899c09"
   license "Apache-2.0"
 
-  depends_on "chaoningkevin/homebrew-tap/qemu@6.1.0"
+  depends_on "qemu@6.1.0"
 
   def install
+    bin.install "src/HelloWorld.go"
     puts "Hello Kevin!"
     puts "ShinySylveon installed"
   end
